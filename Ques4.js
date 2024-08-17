1d642e600d4b5e68eddd4746e8e5ce6fce300752
Md Khalid Alam <sonualam96314@gmail.com>
2024-08-17 07:33:06 +0530

    quest4

---
+++ Ques4.js
@@ -0,0 +1,18 @@
+
+
+// exam-1
+let a = false ; 
+let b = true ; 
+console.log(a || b);
+
+// exam-2
+let c = false ; 
+let d = "md" ;
+console.log(c || d);
+
+// exam-3
+let e = false;
+let f = 5 ;
+let g = 1 ;
+console.log(e || f || g)
+
